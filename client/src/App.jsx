8e1b8bdcda76b97ab866f6d8d478084d9ba72f02
Uyanda Shabalala <uyandashabalala33@gmail.com>
2026-09-05 17:41:54 +0200

    fix : address changes requested in PR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import heroImg from './assets/hero.png';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
@@ -10,8 +6,6 @@ import TeacherDashboard from './pages/teacher/Dashboard';
 import StudentDashboard from './pages/student/Dashboard';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <Routes>
